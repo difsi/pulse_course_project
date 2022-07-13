911b09798dd483744965a67dcc94bcd6b0827b3f
difsi <ttm.frontend@gmail.com>
2022-07-13 17:34:31 +0600

    add settings for carousel

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         speed: 600,
         fade: true,
         cssEase: 'linear',
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>'
       });
 });
